a9ce67187c79933f9c214dac311854990e660cc7
Lloyd Lobo <lloydlobo@gmail.com>
2023-02-05 13:51:18 +0530

    ci: create workspace with xtask & main pkg

---
+++ docs/search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = JSON.parse('{\
+"dashboard":{"doc":"","t":[5],"n":["main"],"q":["dashboard"],"d":[""],"i":[0],"f":[[[]]],"p":[]}\
+}');
+if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
+if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
---
+++ docs/source-files.js
@@ -0,0 +1,4 @@
+var sourcesIndex = JSON.parse('{\
+"dashboard":["",[],["main.rs"]]\
+}');
+createSourceSidebar();
