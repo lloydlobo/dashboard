711e2225f3b83a2897d54302333c3213eac7a881
Lloyd Lobo <lloydlobo@gmail.com>
2023-02-05 14:30:10 +0530

    feat: add path to pkg name & json out
    - Also added comments for 'Result' and 'AppError'

--- dashboard/src/main.rs
+++ dashboard/src/main.rs
@@ -15,6 +15,14 @@ use thiserror::Error;
 
 //------------------------------------------------------------------------------
 
+/// Name of `dashboard` `package`in `/dashboard/Cargo.toml`.
+pub const PKG_NAME: &str = env!("CARGO_PKG_NAME");
+
+/// Path to `gh` cli output for `repo list` command.
+pub const JSON_GH_REPO_LIST: &str = "gh_repo_list.json";
+
+//------------------------------------------------------------------------------
+
 fn main() -> Result<(), AppError> {
     pretty_env_logger::init();
     if let Err(e) = try_main() {
@@ -33,15 +41,21 @@ fn try_main() -> Result<(), AppError> {
 
 //------------------------------------------------------------------------------
 
+/// `Result<T, E>`
+///
+/// This is a reasonable return type to use throughout your application but also
+/// for `fn main`; if you do, failures will be printed along with any
+/// [context](https://docs.rs/anyhow/1.0.69/anyhow/trait.Context.html) and a backtrace if one was captured.
 pub type Result<T, E> = anyhow::Result<T, E>;
 
+/// `AppError`
+///
 /// # Example
 ///
 /// ```ignore
 /// fn simple_err(msg: &str) -> Result<(), anyhow::Error> {
 ///     return Err(anyhow!("MissingAttribute: {}", msg));
 /// }
-///
 /// fn return_err(msg: &str) -> AppError {
 ///     AppError::UnknownWithMsg(msg.to_string())
 /// }
