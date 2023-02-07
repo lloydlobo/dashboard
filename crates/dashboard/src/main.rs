01dfc275e488e4f1c4fe981150b3082c15cb7172
Lloyd Lobo <lloydlobo@gmail.com>
2023-02-07 18:58:20 +0530

    lint: run clippy fix

--- crates/dashboard/src/main.rs
+++ crates/dashboard/src/main.rs
@@ -117,7 +117,7 @@ fn try_main() -> app::Result<(), AppError> {
         .join("\n");
     let block = CommentBlock::new("tag_1".to_string());
     let path = Path::new(PATH_MD_OUTPUT);
-    findrepl::replace(&text, block, &path).map_err(AppError::Parsing)
+    findrepl::replace(&text, block, path).map_err(AppError::Parsing)
 }
 
 //------------------------------------------------------------------------------
@@ -236,9 +236,9 @@ pub mod config {
 
 pub mod db {
 
-    use std::sync::Arc;
+    
 
-    use anyhow::{anyhow, Context};
+    
     use serde::{Deserialize, Serialize};
     use xshell::{cmd, Shell};
 
