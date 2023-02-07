85598aacdf35a42e027d3fc8055fb5c56fffeb8d
Lloyd Lobo <lloydlobo@gmail.com>
2023-02-07 16:47:11 +0530

    docs: update docs with parser crate

---
+++ docs/implementors/core/convert/trait.Into.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"parser":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;Error&gt; for <a class=\"enum\" href=\"parser/enum.ParserError.html\" title=\"enum parser::ParserError\">ParserError</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- docs/source-files.js
+++ docs/source-files.js
@@ -1,6 +1,6 @@
 var sourcesIndex = JSON.parse('{\
 "dashboard":["",[],["main.rs","markdown.rs"]],\
-"parser":["",[],["lib.rs"]],\
+"parser":["",[],["error.rs","lib.rs","macros.rs"]],\
 "xtask":["",[],["main.rs"]]\
 }');
 createSourceSidebar();
