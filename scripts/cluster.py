f27f6e1be78f6e96c7d90fe43068a574da0d70bc
Lloyd Lobo <lloydlobo@gmail.com>
2023-02-18 16:49:42 +0530

    fix: join all topic in each repo

--- scripts/cluster.py
+++ scripts/cluster.py
@@ -1,7 +1,8 @@
 import json
 
-import numpy as np  # Pyright: "np" is not accessed
+import numpy as np
 from nltk.corpus import wordnet as wn
+from scipy.sparse import csr_matrix
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 PATH_PARSED_JSON = "parsed.json"
@@ -25,29 +26,24 @@ def extract_topics(path):
     return topics
 
 
-# Clean up the topics data.
 def clean_topics(topics):
-    some_topics = []
+    """
+    Filtering out any empty topics and return list of non-empty topics for each repository,
+
+    Then generate a corpus of text for the TfidfVectorizer to process. Clean up the topics data.
+    Each element in the corpus is a sentence containing the topic(s) for a single repository.
+    """
+    corpus = []
     for topic in topics:
         if len(topic) > 0:
-            some_topics.append(topic)
-        # else:
-        # some_topics.append(None)
-    return some_topics
-
-
-corpus = [
-    "This is the first document.",
-    "This document is the second document.",
-    "And this is the third one.",
-    "Is this the first document?",
-]
+            corpus.append(" ".join(topic))
+    return corpus
 
 
 # Create feature matrix.
-def process_feature_matrix(topics):
+def process_feature_matrix(corpus):
     vectorizer = TfidfVectorizer()
-    X = vectorizer.fit_transform(topics)
+    X = vectorizer.fit_transform(corpus)
     return X
 
 
@@ -57,24 +53,37 @@ def main():
     topics = extract_topics(PATH_PARSED_JSON)
 
     # Clean up the topics data.
-    some_topics = clean_topics(topics)
-
-    group_topics = []
-    for items in some_topics:
-        for item in items:
-            group_topics.append(item)
-    pass
+    group_topics = clean_topics(topics)
     print(json.dumps(group_topics, indent=4))
 
-    # Create a feature matrix.
-    X = process_feature_matrix(group_topics)
 
-    return X
+#   # Create a feature matrix.
+#   X = process_feature_matrix(group_topics)
+
+#   # Convert the output you provided into a sparse matrix
+#   sparse_matrix = np.array(X)  # sparse_matrix or topic_weights
+#   print(X, sparse_matrix)
+
+#   # Traceback (most recent call last):
+#   #   File "/home/lloyd/p/dashboard/scripts/cluster.py", line 90, in <module>
+#   #     data = main()
+#   #            ^^^^^^
+#   #   File "/home/lloyd/p/dashboard/scripts/cluster.py", line 79, in main
+#   #     for i in range(len(sparse_matrix)):
+#   #                    ^^^^^^^^^^^^^^^^^^
+#   # TypeError: len() of unsized object
+#   # Generate a list of tuples where each tuple contains the document index followed by the indices of the top two topics for that document.
+#   top_topics = []
+#   for i in range(len(sparse_matrix)):
+#       sorted_topics = sorted(
+#           enumerate(sparse_matrix[i]), key=lambda x: x[1], reverse=True
+#       )
+#       top_topics.append((i, sorted_topics[0][0], sorted_topics[1][0]))
+#   pass
+
+#   return X
 
 
 if __name__ == "__main__":
     data = main()
     print(data)
-
-# from typing import List
-# from typing import Optional
